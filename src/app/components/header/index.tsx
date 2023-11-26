e3925e492996616ce1a445d06c5872cec6b85ba6
laggu91 <laggu91@gmail.com>
2023-11-26 11:09:18 +0000

    feat: set layout
    Merge-request: ARECIBO-MR-24
    Merged-by: 구원 나 <laggu91@gmail.com>

---
+++ src/app/components/header/index.tsx
@@ -0,0 +1,9 @@
+export default function Header() {
+    return (
+        // TODO: remove border
+        <div className="w-full border-2 border-black">
+            <p className="text-4xl m-4">Arecibo</p>
+            <hr />
+        </div>
+    )
+}
---
+++ src/app/components/menu/index.tsx
@@ -0,0 +1,8 @@
+export default function Menu() {
+    return (
+        // TODO: remove border
+        <div className="border-2 border-black">
+            <p className="text-4xl m-4">menu</p>
+        </div>
+    )
+}
