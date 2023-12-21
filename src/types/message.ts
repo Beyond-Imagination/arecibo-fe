a0dd420c071f662fb1fa9bdaf9c23ce635593f45
Yoon Jaehyun <yuchem2@gmail.com>
2023-12-21 02:36:26 +0000

    feat: add message types for get message list
    Merge-request: ARECIBO-MR-48
    Merged-by: 재현 윤 <yuchem2@gmail.com>

---
+++ src/types/message.ts
@@ -0,0 +1,26 @@
+export interface IMessage {
+    _id: string
+    title: string
+    content: string
+    author: {
+        organization: string
+        nickname: string
+    }
+    commentCount: number
+    likeCount: number
+    isBlind: boolean
+    createdAt: Date
+    updatedAt: Date
+}
+
+export interface IGetMessageListResponse {
+    messages: IMessage[]
+    page: {
+        totalDocs: number
+        totalPages: number
+        hasNextPage: boolean
+        hasPrevPage: boolean
+        page: number
+        limit: number
+    }
+}
