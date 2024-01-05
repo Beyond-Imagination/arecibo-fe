1287f0a7baac159a99c712a13b5ddeba92a8c863
laggu91 <laggu91@gmail.com>
2024-01-05 14:02:35 +0000

    feat: show planet list
    Merge-request: ARECIBO-MR-69
    Merged-by: 구원 나 <laggu91@gmail.com>

--- src/components/menu/index.tsx
+++ src/components/menu/index.tsx
@@ -1,8 +1,24 @@
+import { useQuery } from 'react-query'
+
+import { useAlien } from '@/providers'
+import { getPlanets } from '@/api'
+
 export default function Menu() {
+    const alien = useAlien()
+
+    const { data } = useQuery(['planets', alien], getPlanets, {
+        enabled: !!alien,
+        refetchOnWindowFocus: false,
+        suspense: true,
+    })
+
     return (
         // TODO: remove border
         <div className="border-2 border-black">
             <p className="text-4xl m-4">menu</p>
+            {/*TODO: planet component 추가. 클릭시 planet page 로 이동*/}
+            {/*TODO: planet 정렬 기준 추가*/}
+            {data?.planets.map((planet) => <div>{planet.title}</div>)}
         </div>
     )
 }
