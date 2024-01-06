a00bfc896226b56bdc2c692241e92aaf94729e33
Yoon Jaehyun <yuchem2@gmail.com>
2024-01-06 07:51:31 +0000

    fix: resolve fe develop error
    Merge-request: ARECIBO-MR-71
    Merged-by: 재현 윤 <yuchem2@gmail.com>

--- src/components/menu/index.tsx
+++ src/components/menu/index.tsx
@@ -18,7 +18,7 @@ export default function Menu() {
             <p className="text-4xl m-4">menu</p>
             {/*TODO: planet component 추가. 클릭시 planet page 로 이동*/}
             {/*TODO: planet 정렬 기준 추가*/}
-            {data?.planets.map((planet) => <div>{planet.title}</div>)}
+            {data?.planets.map(planet => <div key={planet._id}>{planet.title}</div>)}
         </div>
     )
 }
