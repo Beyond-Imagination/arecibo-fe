63330db606bcfe2775b92f8d86f3cb2ae0178b14
laggu91 <laggu91@gmail.com>
2024-01-23 14:55:11 +0000

    feat: show user nickname on header
    Merge-request: ARECIBO-MR-83
    Merged-by: 구원 나 <laggu91@gmail.com>

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,13 +1,21 @@
 import Image from 'next/image'
+import { useAlien } from '@/providers'
 
 export default function Header() {
+    const alien = useAlien()
     return (
         <div>
-            <div className="flex w-full m-8 px-4">
-                <Image src="/images/arecibo2.png" width={64} height={64} alt="arecibo logo" />
-                <p className="text-4xl m-4">Arecibo</p>
+            <div className="flex w-full px-12 py-8 justify-between">
+                <div className="flex">
+                    <Image src="/images/arecibo2.png" width={64} height={64} alt="arecibo logo" />
+                    <p className="text-4xl m-4">Arecibo</p>
+                </div>
+                <div className="flex items-baseline">
+                    <Image src="/images/alien.png" width={48} height={48} alt="Alien Image" />
+                    <p>{alien.nickname}</p>
+                </div>
             </div>
-            <div className="flex-1 mx-8 border-t-2 border-black"></div>
+            <div className="flex-1 mx-8 border-t-2 border-black"></div> {/* 가로선 */}
         </div>
     )
 }
