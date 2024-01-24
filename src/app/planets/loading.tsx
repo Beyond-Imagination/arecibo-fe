210199e8cc0bb37a3f9d835bb2958d9e578c91c7
Yoon Jaehyun <yuchem2@gmail.com>
2024-01-24 12:47:40 +0000

    style: update loading component colors
    Merge-request: ARECIBO-MR-85
    Merged-by: 재현 윤 <yuchem2@gmail.com>

--- src/app/planets/loading.tsx
+++ src/app/planets/loading.tsx
@@ -4,8 +4,8 @@ export default function Loading() {
     return (
         <div className="flex flex-col justify-start w-full min-h-screen p-12">
             <div className="flex flex-row justify-between w-full my-2">
-                <div className="animate-pulse rounded-lg w-8/12 h-10 bg-[#171717]" />
-                <div className="animate-pulse rounded-lg w-3/12 h-10 bg-[#171717]" />
+                <div className="animate-pulse rounded-lg w-8/12 h-10 bg-white dark:bg-[#171717]" />
+                <div className="animate-pulse rounded-lg w-3/12 h-10 bg-white dark:bg-[#171717]" />
             </div>
             <MessageListLoading />
         </div>
