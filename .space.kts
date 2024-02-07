6eacd5c2f5582c8e6267924d54456af38b74970e
laggu91 <laggu91@gmail.com>
2024-02-07 14:39:05 +0000

    chore: prod deploy script
    Merge-request: ARECIBO-MR-125
    Merged-by: 구원 나 <laggu91@gmail.com>

--- .space.kts
+++ .space.kts
@@ -63,3 +63,46 @@ job("[FE] Deploy Develop") {
         }
     }
 }
+
+job("[FE] Deploy Production") {
+    startOn {
+        gitPush {
+            anyBranchMatching {
+                +"main"
+            }
+        }
+    }
+
+    container(displayName = "build", image = "node:alpine") {
+        shellScript {
+            content = """
+                yarn install
+
+                echo "NEXT_PUBLIC_NEWRELIC_AGENT_ID={{ project:NEXT_PUBLIC_NEWRELIC_AGENT_ID_DEV }}" >> .env.development
+                cp .env.development .env
+
+                yarn build
+                cp -r out ${'$'}JB_SPACE_FILE_SHARE_PATH/out
+            """
+        }
+    }
+
+    container(displayName = "deploy", image = "amazon/aws-cli") {
+        env["AWS_ACCESS_KEY_ID"] = "{{ project:AWS_ACCESS_KEY_ID }}"
+        env["AWS_ACCESS_KEY_SECRET"] = "{{ project:AWS_ACCESS_KEY_SECRET }}"
+
+        shellScript {
+            content = """
+                ls -al ${'$'}JB_SPACE_FILE_SHARE_PATH/out
+                aws --version
+
+                export AWS_ACCESS_KEY_ID=${'$'}AWS_ACCESS_KEY_ID
+                export AWS_SECRET_ACCESS_KEY=${'$'}AWS_ACCESS_KEY_SECRET
+                export AWS_DEFAULT_REGION=ap-northeast-2
+
+                aws s3 sync ${'$'}JB_SPACE_FILE_SHARE_PATH/out s3://arecibo-frontend-prod/out
+                aws cloudfront create-invalidation --distribution-id EX5I97GANXD0M --paths "/*"
+            """
+        }
+    }
+}
