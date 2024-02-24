15b82e011999fed3deaee81b4d026ce9dce1a5d5
Yoon Jaehyun <yuchem2@gmail.com>
2024-02-24 02:11:25 +0000

    style: change loading component ui
    Merge-request: ARECIBO-MR-145
    Merged-by: 재현 윤 <yuchem2@gmail.com>

--- src/app/messages/detail/loading.tsx
+++ src/app/messages/detail/loading.tsx
@@ -4,8 +4,7 @@ export default function Loading() {
     return (
         <div className="flex flex-col justify-start w-full min-h-screen p-12">
             <div className="flex flex-row justify-between w-full my-2">
-                <div className="animate-pulse rounded-lg w-8/12 h-10 bg-white dark:bg-[#171717]" />
-                <div className="animate-pulse rounded-lg w-3/12 h-10 bg-white dark:bg-[#171717]" />
+                <div className="animate-pulse rounded-lg w-full h-9 bg-neutral-300 dark:bg-[#ffffff26]" />
             </div>
             <MessageLoading />
         </div>
--- src/components/loading.tsx
+++ src/components/loading.tsx
@@ -14,54 +14,72 @@ export function AuthLoading() {
 }
 
 export function TextEditorLoading() {
-    return <div className="border rounded-lg animate-pulse w-full h-28 p-2 bg-[#E3E3E3]" />
+    return <div className="border rounded-lg animate-pulse w-full h-28 p-2 bg-neutral-300 dark:bg-[#ffffff26]" />
 }
 
 function MessageSkeleton() {
     return (
-        <div className="block rounded-lg m-2 bg-neutral-200 dark:bg-neutral-700">
-            <div className="flex flex-row justify-between w-full">
-                <div className="flex flex-row items-center justify-start align-center w-full my-1">
-                    <div className="rounded-lg w-6 h-6 mx-2 bg-neutral-300 dark:bg-neutral-600" />
-                    <div className="rounded-lg w-4/12 h-6 bg-neutral-300 dark:bg-neutral-600" />
+        <div className="border-2 block rounded-lg mb-3 p-6">
+            <div className="animate-pulse pt-1 w-full">
+                <div className="rounded-lg h-32 bg-neutral-300 dark:bg-[#ffffff26]" />
+                <div className="flex flex-row justify-start mt-2">
+                    <div className="rounded-3xl w-28 h-10 me-3 bg-neutral-300 dark:bg-[#ffffff26]" />
+                    <div className="rounded-3xl w-28 h-10 bg-neutral-300 dark:bg-[#ffffff26]" />
                 </div>
-                <div className="rounded-lg w-2/12 h-6 m-1 bg-neutral-300 dark:bg-neutral-600" />
             </div>
-            <div className="pt-1 w-full">
-                <div className="rounded-lg h-6 m-2 bg-neutral-300 dark:bg-neutral-600" />
-                <div className="rounded-lg h-32 m-2 bg-neutral-300 dark:bg-neutral-600" />
+        </div>
+    )
+}
+
+function CommentSkeleton() {
+    return (
+        <div className="border-l-2 w-full px-4 mb-4">
+            <div className="animate-pulse rounded-lg h-24 w-full bg-neutral-300 dark:bg-[#ffffff26]" />
+            <div className="border-l-2 w-full px-4 mx-2 mt-2">
+                <div className="animate-pulse rounded-lg h-24 w-full bg-neutral-300 dark:bg-[#ffffff26]" />
             </div>
-            <div className="flex flex-row justify-start w-full pt-2">
-                <div className="rounded-lg w-2/12 h-6 m-2 bg-neutral-300 dark:bg-neutral-600" />
-                <div className="rounded-lg w-2/12 h-6 m-2 bg-neutral-300 dark:bg-neutral-600" />
-            </div>
+        </div>
+    )
+}
+
+function CommentListSkeleton() {
+    return (
+        <div className="flex flex-col border-t-2 items-center py-4 px-4">
+            <CommentSkeleton />
+            <CommentSkeleton />
         </div>
     )
 }
 
 export function MessageLoading() {
     return (
-        <div className="animate-pulse flex flex-col rounded-lg w-full mt-2 p-2 space-y-2 bg-white dark:bg-neutral-900">
+        <div className="flex flex-col">
             <MessageSkeleton />
-            <div className="rounded-lg h-8 m-2 pt-2 bg-neutral-200 dark:bg-neutral-700" />
+            <div className="border-2 block rounded-lg my-3 p-3">
+                <div className="animate-pulse rounded-lg w-full h-24 mb-3 bg-neutral-300 dark:bg-[#ffffff26]" />
+                <div className="flex flex-row justify-end">
+                    <div className="animate-pulse rounded-lg w-32 h-8 mb-5 bg-neutral-300 dark:bg-[#ffffff26]" />
+                </div>
+                <CommentListSkeleton />
+            </div>
         </div>
     )
 }
 
 export function MessageListLoading() {
     return (
-        <div className="animate-pulse flex flex-col rounded-lg w-full mt-2 p-2 space-y-2 bg-white dark:bg-neutral-900">
+        <div className="animate-pulse flex flex-col rounded-lg w-full mt-2">
             <MessageSkeleton />
             <MessageSkeleton />
-            <div className="rounded-lg h-8 m-2 pt-2 bg-neutral-200 dark:bg-neutral-700" />
+            <MessageSkeleton />
         </div>
     )
 }
 
 export function CommentFormLoading() {
     return (
-        <div className="flex flex-col rounded items-center w-full h-16 bg-white">
-            <FadeLoader color="#646464" />
+        <div className="border flex flex-col rounded-b items-center w-full h-16 bg-white dark:bg-[#18191b]">
+            <FadeLoader color="#666666" />
         </div>
     )
 }
