c31dbb28c03e3e79f7b41b7e94cdfab356f5b75b
chsjen492 <chsjen492@gmail.com>
2024-03-28 16:08:30 +0000

    feat: add button to move planets subscribe page

--- src/components/menu/index.tsx
+++ src/components/menu/index.tsx
@@ -1,10 +1,12 @@
 import { useEffect } from 'react'
 import { useSuspenseQuery } from '@tanstack/react-query'
 import { useRouter, usePathname } from 'next/navigation'
+import Link from 'next/link'
 
 import { useAuthorization } from '@/providers'
 import { getSubscribedPlanets } from '@/api'
 import PlanetLink from '@/components/menu/planetLink'
+import { Plus } from '@/icon'
 
 export default function Menu() {
     const auth = useAuthorization()
@@ -30,7 +32,12 @@ export default function Menu() {
     return (
         <div className="w-1/4 min-h-full pt-6">
             <div className="border-t-2"></div>
-            <p className="text-4xl m-2">Planet</p>
+            <div className="flex items-center">
+                <p className="w-10/12 text-4xl m-2">Planet</p>
+                <Link href={'/planets/subscribe'} className="m-2">
+                    <Plus />
+                </Link>
+            </div>
             <div className="border-t-2"></div>
             {/*TODO: planet 정렬 기준 추가*/}
             {data?.planets.map(planet => <PlanetLink key={planet._id} planet={planet} />)}
